49462ea0a2673237db268baceafd51f4440f3971
RafaelMendonca <rafael.medonca2@gmail.com>
2023-04-20 08:50:20 -0300

    primeiro commit

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using FilmesApi.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
+    {
+        
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
---
+++ FilmesApi/Entity/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Entity;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Titulo do filme é obrigatorio")] 
+    public string Titulo { get; set; }
+    [Required(ErrorMessage ="O genero do filme é obrigatorio")]
+    [MaxLength(50)]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 300 , ErrorMessage ="A duração deve ter entre 70 e 300 minutos")]
+    public int Duracao { get; set; }
+}
